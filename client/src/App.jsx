aacb54c5db0d09534e3c7e5a212c9f28aaf90213
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-27 18:57:41 +0500

    add: react router dom with some routes as well

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 
-function App() {
-
+function App () {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
