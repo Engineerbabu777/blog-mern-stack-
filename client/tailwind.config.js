302b718aee06771da97dd9a8cb8c97bea1a4489e
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-28 18:48:56 +0500

    add: ulta completeion

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
@@ -10,5 +10,6 @@ export default {
   },
   plugins: [
     require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
   ],
-}
+};
